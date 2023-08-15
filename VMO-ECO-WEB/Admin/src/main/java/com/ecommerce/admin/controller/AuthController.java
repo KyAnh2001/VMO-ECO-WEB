42246b2f99fbb5d905d141735452acf19124cd5d
KyAnhND2001 <kyanhnd2001@gmail.com>
2023-08-15 13:56:14 +0700

    final

---
+++ VMO-ECO-WEB/Admin/src/main/java/com/ecommerce/admin/controller/AuthController.java
@@ -0,0 +1,110 @@
+package com.ecommerce.admin.controller;
+
+import com.ecommerce.library.dto.AdminDto;
+import com.ecommerce.library.dto.ProductDto;
+import com.ecommerce.library.model.Admin;
+import com.ecommerce.library.model.Product;
+import com.ecommerce.library.service.AdminService;
+import com.ecommerce.library.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+    private final AdminService adminService;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+    private ProductService productService;
+
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "Login Page");
+        return "login";
+    }
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("title", "Home Page");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "redirect:/login";
+        }
+        return "index";
+    }
+
+    @GetMapping("/admin/index")
+    public String showAdminIndexPage(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        model.addAttribute("size", products.size());
+        return "/index";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Register");
+        model.addAttribute("adminDto", new AdminDto());
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword(Model model) {
+        model.addAttribute("title", "Forgot Password");
+        return "forgot-password";
+    }
+
+    @PostMapping("/register-new")
+    public String addNewAdmin(@Valid @ModelAttribute("adminDto") AdminDto adminDto,
+                              BindingResult result,
+                              Model model) {
+
+        try {
+
+            if (result.hasErrors()) {
+                model.addAttribute("adminDto", adminDto);
+                result.toString();
+                return "register";
+            }
+            String username = adminDto.getUsername();
+            Admin admin = adminService.findByUsername(username);
+            if (admin != null) {
+                model.addAttribute("adminDto", adminDto);
+                System.out.println("admin not null");
+                model.addAttribute("emailError", "Your email has been registered!");
+                return "register";
+            }
+            if (adminDto.getPassword().equals(adminDto.getRepeatPassword())) {
+                adminDto.setPassword(passwordEncoder.encode(adminDto.getPassword()));
+                adminService.save(adminDto);
+                System.out.println("success");
+                model.addAttribute("success", "Register successfully!");
+                model.addAttribute("adminDto", adminDto);
+            } else {
+                model.addAttribute("adminDto", adminDto);
+                model.addAttribute("passwordError", "Your password maybe wrong! Check again!");
+                System.out.println("password not same");
+                return "register";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("errors", "The server has been wrong!");
+        }
+        return "register";
+
+    }
+}
